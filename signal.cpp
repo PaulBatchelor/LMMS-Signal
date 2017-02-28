589cb37d73d1fc51de73151e06edc29fcc94a221
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-02-27 20:02:34 -0800

    renamed files

--- signal.cpp
+++ signal.cpp
@@ -1,38 +1,8 @@
-/*
- * lb302.cpp - implementation of class lb302 which is a bass synth attempting
- *             to emulate the Roland TB303 bass synth
- *
- * Copyright (c) 2006-2008 Paul Giblock <pgib/at/users.sourceforge.net>
- *
- * This file is part of LMMS - https://lmms.io
- *
- * lb302FilterIIR2 is based on the gsyn filter code by Andy Sloane.
- *
- * lb302Filter3Pole is based on the TB303 instrument written by
- *   Josep M Comajuncosas for the CSounds library
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program (see COPYING); if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA.
- *
- */
-
 // Need to include this first to ensure we get M_PI in MinGW with C++11
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-#include "lb302.h"
+#include "signal.h"
 #include "AutomatableButton.h"
 #include "Engine.h"
 #include "InstrumentPlayHandle.h"
